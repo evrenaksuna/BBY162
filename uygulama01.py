58581c7d7446a3cd5c9d066d800a82cb8ea0beab
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2018-02-24 13:16:08 +0300

    Update uygulama01.py

--- uygulama01.py
+++ uygulama01.py
@@ -1,3 +1,4 @@
+#metnin yarısını yazdıran
 uygulama_bir = "Einstein did not reject the existence of time. Instead, he rejected the distinction between past, present, and future."
 #print(len(uygulama_bir)) ===118
 print(uygulama_bir[0:64])
