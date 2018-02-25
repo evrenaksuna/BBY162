e5748a969232deee32a31bee79d9016473995f03
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2018-02-25 13:23:18 +0300

    Update uygulama01.py

--- uygulama01.py
+++ uygulama01.py
@@ -1,4 +1,9 @@
 #metnin yarısını yazdıran
+metin = input("Metni gir.:")
+metninyarisi = metin[: int(len(metin)/2)]
+print(metninyarisi) 
+
+
 uygulama_bir = "Einstein did not reject the existence of time. Instead, he rejected the distinction between past, present, and future."
 #print(len(uygulama_bir)) ===118
 print(uygulama_bir[0:64])
