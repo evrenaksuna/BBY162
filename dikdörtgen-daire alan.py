2937c2afb5c4e290183a5a01507aafe5b026023f
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2018-02-25 17:58:09 +0300

    Update dikdörtgen-daire alan.py

--- dikdörtgen-daire alan.py
+++ dikdörtgen-daire alan.py
@@ -1,4 +1,3 @@
-#dikdörtgen alan
 def dortgen_alan_hesapla_vl(a,b):
     alan = a*b
     print("alan=", alan)
@@ -6,10 +5,23 @@ a = int(input("Dikdörtgenin kısa kenarı:"))
 b = int(input("Dikdörtgenin uzun kenarı:"))
 dortgen_alan_hesapla_vl(a,b)
 
+def daire_alan_hesapla_v1(pisayisi,r):
+    alan = pisayisi*(r**2)
+    print("alan=", alan)
+pisayisi= 3.14
+r= int(input("Dairenin yarıçapı="))
+daire_alan_hesapla_v1(pisayisi,r)
 
-#daire alan
-yaricap = int(input("Dairenin yarıçapı="))
-r = yaricap
+def dortgen_alan_hesapla_v2(a,b):
+    alan = a**2 * b**2
+    print("alan=", alan)
+a = int(input("Dikdörtgenin kısa kenarı:"))
+b = int(input("Dikdörtgenin uzun kenarı:"))
+dortgen_alan_hesapla_v2(a,b)
+
+def daire_alan_hesapla_v2(pisayisi,r):
+    alan = pisayisi * (r**2)**2
+    print("Dairenin alanı=", alan)
 pisayisi = 3.14
-alan = pisayisi*(r**2)
-print("Dairenin alanı=", alan)
+r = int(input("Dairenin yarıçapı="))
+daire_alan_hesapla_v2(pisayisi,r)
