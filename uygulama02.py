db7cdaf7c6c0ec3f5036aab9810838a2e9ff025f
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2018-02-25 15:52:06 +0300

    Update uygulama02.py

--- uygulama02.py
+++ uygulama02.py
@@ -1,53 +1,25 @@
 #saniyeyi dk,sa,gun vs çeviren
-def timechanger(zaman):
-    if zaman < 60:
-         print("zaman :%d saniye"%(zaman))
-    elif 60<=zaman<3600:
-         dakika = zaman/60
-         saniye = zaman%60
-         print("zaman :%d dakika %d saniye"%(dakika ,saniye))
-    elif  3600<=zaman<=86400:
-        saat = zaman/3600
-        zaman = zaman -saat*3600
-        dakika = zaman/60
-        saniye = zaman%60
-        print("zaman :%d saat %d dakika %d saniye"%(saat,dakika ,saniye))
-    elif 86400<=zaman<=604800:
-        gun = zaman/86400
-        saat = zaman/3600
-        zaman = zaman -saat*3600
-        saat = zaman%3600
-        dakika = zaman/60
-        saniye = zaman%60
-        print("zaman :%d gun %d saat %d dakika %d saniye" % (gun, saat, dakika, saniye))
-    elif 604800<=zaman<=2592000:
-        hafta = zaman/ 604800
-        gun = zaman/ 86400
-        saat = zaman/ 3600
-        zaman = zaman -saat * 3600
-        gun=zaman%86400
-        saat = zaman%3600
-        dakika = zaman / 60
-        saniye = zaman % 60
-        print("zaman :%d hafta %d gun %d saat %d dakika %d saniye" % (hafta, gun, saat, dakika, saniye))
-    elif 2592000<=zaman<=31556926:
-        ay = zaman / 2592000
-        hafta = zaman / 604800
-        gun = zaman / 86400
-        saat = zaman / 3600
-        zaman = zaman - saat * 3600
-        gun = zaman % 86400
-        saat = zaman % 3600
-        dakika = zaman / 60
-        saniye = zaman % 60
-        ay= zaman%2592000
-        print("zaman :%d ay %d hafta %d gun %d saat %d dakika %d saniye" % (ay, hafta, gun, saat, dakika, saniye))
+#1 yıl 365 gün olarak alındı
+#1 ay 30 gün olarak alındı
 
-    else:
-        print("geçerli bir zaman gir.")
-while True:
-    ten=input("dönüştürmek istediğin zamanı saniye cinsinden gir:")
-    if ten.isdigit():
-        break
-ten=int(ten)
-timechanger(ten)
+zaman = float(input("Saniye cinsinden zamanı gir: "))
+yıl = zaman // (8760 * 3600)
+zaman = zaman % (8760 * 3600)
+
+ay = zaman // (720 * 3600)
+zaman = zaman % (720 * 3600)
+
+hafta = zaman // (168 * 3600)
+zaman = zaman % (168 * 3600)
+
+gun = zaman // (24 * 3600)
+zaman = zaman % (24 * 3600)
+
+saat = zaman // 3600
+zaman %= 3600
+
+dakika = zaman // 60
+zaman %= 60
+
+saniye = zaman
+print("%d yıl, %d ay, %d hafta, %d gun, %d saat, %d dakika, %d saniye. " % (yıl, ay, hafta, gun, saat, dakika, saniye))
