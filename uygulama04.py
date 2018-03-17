7472c59f692256022c48b7795082a8de0962bc96
Evren Aksuna <36502067+evrenaksuna@users.noreply.github.com>
2018-03-17 16:12:56 +0300

    Update uygulama04.py

--- uygulama04.py
+++ uygulama04.py
@@ -1,14 +1,55 @@
-kadinismi = input("Bir kadın adı giriniz :")
-print(kadinismi)
-erkekismi = input("Bir erkek adı giriniz :")
-print(erkekismi)
-mısra= int(input("Mısra sayısı giriniz :"))
+kim = input("Erkek ismi giriniz:")
+kiminle = input("Kadın ismi giriniz:")
+mısra = input("Mısra sayısı giriniz:")
+sec = int(mısra)
+boşluk = " "
+nerede = open("nerede.txt", "r")
+neyaptı = open("yüklem.txt", "r")
+import random
+def kelime1():
+    secim = random.choice(nerede)
+    nerede.remove(secim)
+    return secim
+def kelime2():
+    secim = random.choice(neyaptı)
+    neyaptı.remove(secim)
+    return secim
 
+a=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+b=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+c=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+d=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+e=("\n")
+f=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+g=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+h=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
+ı=(kim+boşluk+kiminle+boşluk+kelime1()+boşluk+kelime2()+"\n")
 
-sarki = [erkekismi + "Ben ask nedir bilmem "+ kadinismi +" eski kafalıyım ", "bir seni bilirim, bir de adın geçince sıkışan kalbimi" + kadinismi ,"Ben seni bir okyanusun derinliğinde buldum da sevdim ","Parlak bir inciydin benim için.", erkekismi + "paha biçilmez bir inci" + kadinismi + " ben seni soğuk ve yağmurlu bir günde", "Seni düşünürken gülüşündeki sıcaklığın içinde dolup ta" +kadinismi + ",Beni sardığı bir anda sevdim","Seni sadece selvi boyun, siyah saçların ya da kara gözlerin", "Güzel bir yüzün var diye değil" + kadinismi + "Fikirlerinle, konuşmandaki güzelliğin ve benim o kor halde yanan",erkekismi + "yüreğimle sevdim" + kadinismi + "Ben seni derinden" + kadinismi + "hissederek sevdim.."]
-
-for olusturulacak_sarki in sarki[:mısra]:
-    print(olusturulacak_sarki)
-
-    if mısra > 10:
-        print("geçerli bir mısra sayısı giriniz:")
+while True:
+    if sec==1:
+        print(e,a)
+        print
+    elif sec==2:
+        print(e,a,b)
+        print
+    elif sec==3:
+        print(e,a,b,c)
+        print
+    elif sec==4:
+        print(e,a,b,c,d)
+        print
+    elif sec==5:
+        print(e,a,b,c,d,e,f)
+        print
+    elif sec==6:
+        print(e,a,b,c,d,e,f,g)
+        print
+    elif sec==7:
+        print(e,a,b,c,d,e,f,g,h)
+        print
+    elif sec==8:
+        print(e,a,b,c,d,e,f,g,h,ı)
+        print
+    else:
+        print("Maximum mısra sayısını aştınız..")
+    break
